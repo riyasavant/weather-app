9140343099f8275760aae4bbd1b6ea77a59ddcfb
riyasavant <rieee2110@gmail.com>
2020-07-06 23:20:08 +0530

    Added the Open forecast API

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const app = express();
+const https = require('https');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-    res.send("<h1>Hello, world!</h1>");
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post('/', function(req, res) {
+    const city = req.body.city;
+    const apiKey = "4b788fe4a8add403143999519176c16b";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+
+    https.get(url, function(response) {
+        console.log('Status code: ' + response.statusCode);
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const imageUrl = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png";
+            const description = weatherData.weather[0].description;
+            res.write("<h1>" + city + "</h1>")
+            res.write("<h3>Temperature in celsius: " + temp + "</h3>");
+            res.write("<h3>" + description + "</h3>");
+            res.write("<img src="+ imageUrl + ">");
+            res.send();
+        })
+    })
 })
 
 app.listen(3000, function() {
